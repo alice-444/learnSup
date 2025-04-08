11086a5adf23db0acc9114574765dee955a99c6c
Alice <carole.2002@outlook.com>
2025-04-08 11:55:22 +0200

    verification connection

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,17 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Toaster } from "react-hot-toast";
 import { ClerkProvider } from "@clerk/nextjs";
+import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
 
 export const metadata: Metadata = {
-  title: "Learn Sup",
-  description: "next app",
+  title: "LearnSup",
+  description: "app",
+  icons: {
+    icon: "/logo/logo.png",
+    shortcut: "/logo/logo.png",
+    apple: "/logo/logo.png",
+  },
 };
 
 const RootLayout = ({
@@ -17,13 +24,15 @@ const RootLayout = ({
     <ClerkProvider>
       <html lang="en">
         <body>
+          <Navbar />
           {children}
           <Toaster
             position="top-center"
             toastOptions={{
-              duration: 4000,
+              duration: 3000,
             }}
           />
+          <Footer />
         </body>
       </html>
     </ClerkProvider>
